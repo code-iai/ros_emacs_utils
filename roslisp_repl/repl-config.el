2b320698c64c8568180c3952dac64140ea8ec7af
Gayane Kazhoyan <kazhoyan@cs.uni-bremen.de>
2014-05-09 18:27:48 +0200

    [roslisp_repl] updated package.xml and repl-config.el:
    repl-config.el now uses the ROS infrastructure to find paths to other .el modules

--- roslisp_repl/repl-config.el
+++ roslisp_repl/repl-config.el
@@ -1,11 +1,5 @@
 
-(defun ros-package-path (pkg)
-  (save-excursion
-    (with-temp-buffer
-      (call-process "rospack" nil t nil "find" pkg)
-      (goto-char (point-min))
-      (re-search-forward "^\\(.*\\)$")
-      (match-string 1))))
+;; general config
 
 (customize-set-variable 'indent-tabs-mode nil)
 (setq default-tab-width 2)
@@ -26,17 +20,58 @@
                 ("\\.launch"   . xml-mode)) auto-mode-alist))
 (autoload 'mwheel-install "mwheel" "Enable mouse wheel support.")
 
-(require 'slime)
+;; rosemacs
 
+(defun ros-package-path (pkg)
+  (save-excursion
+    (with-temp-buffer
+      (call-process "rospack" nil t nil "find" pkg)
+      (goto-char (point-min))
+      (re-search-forward "^\\(.*\\)$")
+      (match-string 1))))
+
+(add-to-list 'load-path (ros-package-path "rosemacs"))
 (require 'rosemacs)
+
 (invoke-rosemacs)
 (global-set-key "\C-x\C-r" ros-keymap)
+(setq ros-completion-function (quote ido-completing-read))
 
-(slime-setup '(slime-fancy slime-asdf slime-indentation slime-ros))
+;; slime
+
+;; the following will go away, it's just needed for from source emacs installs
+(add-to-list 'load-path "~/workspace/lisp/slime")
+
+(require 'slime-autoloads)
+(setq slime-backend "swank-loader.lisp")
+(add-hook 'inferior-lisp-mode-hook (lambda () (inferior-slime-mode 1)))
+
+(setq inferior-lisp-program "/usr/bin/sbcl --dynamic-space-size 4096")
+(setq slime-lisp-implementations nil)
+
+(add-to-list 'load-path (ros-package-path "slime_ros"))
+(setq slime-contribs '(slime-repl
+                       slime-autodoc
+                       ;; slime-c-p-c
+                       slime-editing-commands
+                       slime-fancy-inspector
+                       slime-fancy-trace
+                       slime-fuzzy
+                       slime-presentations
+                       slime-scratch
+                       slime-references
+                       slime-package-fu
+                       slime-fontifying-fu
+                       slime-trace-dialog
+                       ;;
+                       slime-asdf
+                       slime-indentation
+                       slime-xref-browser
+                       slime-highlight-edits
+                       slime-ros))
+
 (setq slime-complete-symbol-function 'slime-fuzzy-complete-symbol)
 
-(setq slime-multiprocessing t)
-
-(slime-ros)
+(slime)
 
 (provide 'repl-config)
