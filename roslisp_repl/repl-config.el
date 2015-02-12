2ecb2ec184e6b3792f6ef5737b1b36a4219e2d96
Gayane Kazhoyan <kazhoyan@cs.uni-bremen.de>
2015-02-12 16:24:29 +0100

    [roslisp_repl] added some new key bindings:
    e.g., now TAB is bound to slime-fuzzy-indent-and-complete-symbol which does autocompletion.

--- roslisp_repl/repl-config.el
+++ roslisp_repl/repl-config.el
@@ -15,9 +15,57 @@
 ;; It's path is passed to emacs through the -L argument of
 ;; the ``roslisp_repl`` executable.
 (require 'slime-config)
+
 ;; The following gets rid of the gray highlighting of uncompiled code
 ;; that can be confusing and annoying for the beginner Lispers.
 (add-hook 'slime-mode-hook (lambda () (slime-highlight-edits-mode 0)))
+
+;; some key bindings to, e.g., enable easy autocompletion etc.
+(eval-after-load 'slime
+  '(progn
+     ;; Fix for M-, when using it with dired and A
+     (define-key slime-mode-map (kbd "M-,")
+       (lambda ()
+         (interactive)
+         (condition-case nil
+             (slime-pop-find-definition-stack)
+           (error (tags-loop-continue)))))
+     (global-set-key "\C-cs" 'slime-selector)
+     (define-key slime-repl-mode-map (kbd "C-M-<backspace>")
+       'slime-repl-delete-current-input)
+     (define-key slime-mode-map "\r" 'newline-and-indent)
+     (define-key slime-mode-map [tab]
+       (lambda ()
+         (interactive)
+         (let ((yas-fallback-behavior nil))
+           (unless (yas-expand)
+             (slime-fuzzy-indent-and-complete-symbol)))))
+     (define-key slime-mode-map (kbd "M-a")
+       (lambda ()
+         (interactive)
+         (let ((ppss (syntax-ppss)))
+           (if (nth 3 ppss)
+               (goto-char (1+ (nth 8 ppss)))
+             (progn
+               (backward-up-list 1)
+               (down-list 1))))))
+     (define-key slime-mode-map (kbd "M-e")
+       (lambda ()
+         (interactive)
+         (let ((ppss (syntax-ppss)))
+           (if (nth 3 ppss)
+               (progn
+                 (goto-char (nth 8 ppss))
+                 (forward-sexp 1)
+                 (backward-char 1))
+             (progn
+               (up-list 1)
+               (backward-down-list 1))))))))
+
+;;; [ and ] should be handled paranthesis-like in lisp files.
+(modify-syntax-entry ?\[ "(]  " lisp-mode-syntax-table)
+(modify-syntax-entry ?\] ")[  " lisp-mode-syntax-table)
+
 (slime)
 
 ;;; Footer
